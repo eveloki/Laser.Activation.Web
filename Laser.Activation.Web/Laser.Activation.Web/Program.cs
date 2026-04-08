333418eae527d3ea5d84b9eeb47fb16171ba3381
eveloki <eveloki@163.com>
2026-04-08 21:41:26 +0800

    添加项目文件。

---
+++ Laser.Activation.Web/Laser.Activation.Web/Program.cs
@@ -0,0 +1,31 @@
+using Laser.Activation.Web.Client.Pages;
+using Laser.Activation.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Laser.Activation.Web.Client._Imports).Assembly);
+
+app.Run();
